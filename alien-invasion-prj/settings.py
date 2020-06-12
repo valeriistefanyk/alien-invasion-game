f283e7ea7ded359dda8f89fae7ded460d4699089
Valerii Stefanyk <valeriistefanyk@gmail.com>
2020-06-12 17:01:57 +0300

    Init Alien Invasion Game

---
+++ alien-invasion-prj/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """ Класс для хранения всех настроек игры Alien Invasion."""
+
+    def __init__(self):
+        """ Инициализация настроек игры """
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
