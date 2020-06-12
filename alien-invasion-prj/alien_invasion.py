0b8d50605a3629a1a6621b06004ac76428b45cb2
Valerii Stefanyk <valeriistefanyk@gmail.com>
2020-06-12 17:49:59 +0300

    Add image to surface

--- alien-invasion-prj/alien_invasion.py
+++ alien-invasion-prj/alien_invasion.py
@@ -5,6 +5,7 @@ Alien Invasion
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
@@ -13,12 +14,14 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
+    ship = Ship(screen)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
         pygame.display.flip()
 
 
