0a8f9f9d484a6556057aca4937ad192cfdaae019
Valerii Stefanyk <valeriistefanyk@gmail.com>
2020-06-13 14:46:53 +0300

    Add firing mechanics

--- alien-invasion-prj/alien_invasion.py
+++ alien-invasion-prj/alien_invasion.py
@@ -6,6 +6,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 
 def run_game():
@@ -15,11 +16,13 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
     ship = Ship(screen, ai_settings)
+    bullets = Group()
 
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
