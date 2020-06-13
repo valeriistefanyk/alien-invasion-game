fc9de61af2f1b3e47b45f42071624f1e31ee3d3c
Valerii Stefanyk <valeriistefanyk@gmail.com>
2020-06-13 13:55:18 +0300

    Add moving code

--- alien-invasion-prj/alien_invasion.py
+++ alien-invasion-prj/alien_invasion.py
@@ -14,10 +14,11 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
-    ship = Ship(screen)
+    ship = Ship(screen, ai_settings)
 
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 
--- alien-invasion-prj/ship.py
+++ alien-invasion-prj/ship.py
@@ -6,13 +6,25 @@ import pygame
 
 
 class Ship:
-    def __init__(self, screen):
+    def __init__(self, screen, ai_settings):
         self.screen = screen
+        self.ai_settings = ai_settings
         self.image = pygame.image.load('alien-invasion-prj/src/lavr.png')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        self.center = float(self.rect.centerx)
+
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.centerx += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.rect.centerx -= self.ai_settings.ship_speed_factor
+        # self.rect.centerx = self.center
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
